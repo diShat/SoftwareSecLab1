d331eee11c204347bcefc56603688c7364e0e49d
diShat <diana.shatkovska@gmail.com>
2022-04-14 23:04:13 +0200

    users list

--- views/users/users_view.py
+++ views/users/users_view.py
@@ -1,11 +1,23 @@
-from tkinter import Label
+from tkinter import *
+from tkinter import ttk
 
 import window
 
 
 def show(users):
     root = window.get_clean_root()
-    Label(root, text=f"Users:").grid(row=0, column=0)
-    Label(root, text=f"Users:").grid(row=0, column=0)
-    for i, user in enumerate(users):
-        Label(root, text=f"{user.username}").grid(row=i+2, column=0)
+
+    headers = ['Id', 'Username', 'Admin', 'Blocked', 'Pass Restricted']
+    for i, header in enumerate(headers):
+        Label(root, text=f'{header}').grid(row=0, column=i)
+
+    for row, user in enumerate(users):
+        Label(root, text=f'{user.id}').grid(row=row+1, column=0)
+        Label(root, text=f'{user.username}').grid(row=row+1, column=1)
+        Label(root, text=f'{user.is_admin}').grid(row=row+1, column=2)
+        Label(root, text=f'{user.is_blocked}').grid(row=row+1, column=3)
+        Label(root, text=f'{user.is_pass_restricted}').grid(row=row+1, column=4)
+        # if user.is_blocked : Button(root, text="Unblock",
+        #                             command=lambda: unblockuser_handler(user.username)).grid(row=row+1, column=5)
+        # else: Button(root, text="Block",
+        #              command=lambda: blockuser_handler(user.username)).grid(row=row+1, column=5)
